e063ce9ccbe9e017c7ebfa785f5e02cfd1ebbc07
Julianna Green & Janice Dunn <student@epicodus.com>
2015-09-28 12:15:49 -0700

    add listing route

--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('category', {path: '/category/:category_id'});
+  this.route('listing', {path: '/listing/:listing_id'});
 });
 
 export default Router;

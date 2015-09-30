54ce220c91640d5be3fd2c041c8eee212d8c609b
Janice V. Dunn <janicedunn53@gmail.com>
2015-09-30 16:23:34 -0700

    Show 'Add New Listing' form only when clicked.

--- app/components/add-listing-tile.js
+++ app/components/add-listing-tile.js
@@ -1,17 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    actions: {
-      addListing() {
-        var params = {
-          title: this.get('title'),
-          user: this.get('user'),
-          details: this.get('details'),
-          location: this.get('location'),
-          category: this.get('category'),
-          date_added: new Date(),
-        };
-        this.sendAction('addListing', params);
-      }
+  addNewListing: false,
+
+  actions: {
+    listingFormShow() {
+      this.set('addNewListing', true)
+    },
+    addListing() {
+      var params = {
+        title: this.get('title'),
+        user: this.get('user'),
+        details: this.get('details'),
+        location: this.get('location'),
+        category: this.get('category'),
+        date_added: new Date(),
+      };
+      this.set('addNewListing', false),
+      this.sendAction('addListing', params);
     }
+  }
 });
